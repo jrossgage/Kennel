4e2dc95b9900946344e3e67e63db0e0f17f44e5c
Joel <jrossgage@gmail.com>
2021-04-01 22:07:44 -0500

    Created repository

---
+++ src/components/Customer/Customer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Customer.css";
+
+export const CustomerCard = () => (
+    <section className="customer">
+        <h3 className="customer__name">Fredrick</h3>
+        <div className="customer__address">100 Here Dr.</div>
+    </section>
+)
