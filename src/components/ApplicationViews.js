aab8550a0a95f0bfcd89d64a28d69de1f059bd4a
Joel <jrossgage@gmail.com>
2021-04-07 15:12:39 -0500

    Detail page works for Animals

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -5,6 +5,10 @@ import { AnimalList } from "./animal/AnimalList"
 import { LocationList } from "./Location/LocationList"
 import { CustomerList } from "./Customer/CustomerList"
 import { EmployeeList } from "./Employee/EmployeeList"
+import { AnimalDetail } from "./animal/AnimalDetail"
+import { LocationDetail } from "./Location/LocationDetail"
+import { AnimalForm } from "./animal/AnimalForm";
+
 
 export const ApplicationViews = () => {
     return (
@@ -15,14 +19,26 @@ export const ApplicationViews = () => {
             </Route>
 
             {/* Render the animal list when http://localhost:3000/animals */}
-            <Route path="/animals">
+            <Route exact path="/animals">
             <h2>Animals</h2>
               <AnimalList />
             </Route>
 
-            <Route path="/locations">
+            <Route path="/animals/:animalId(\d+)">
+                <AnimalDetail />
+            </Route>
+
+            <Route path="/animals/create">
+                <AnimalForm />
+            </Route>
+            
+            <Route exact path="/locations">
             <h2>Locations</h2>
             <LocationList />
+            </Route>
+
+            <Route path="/locations/:locationId(\d+)">
+                <LocationDetail />
             </Route>
 
             <Route path="/customers">
